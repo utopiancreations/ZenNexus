cceb21d9cc34b5fe2f83609546d223e912c7e3ed
utopiancreations <jmiller@utopiancreations.one>
2025-09-13 10:12:11 -0700

    Initial commit: Project setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,18 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  adapter: vercel(),
 });
